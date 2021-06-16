8ec66c9d6182fcec2eaf32193037bdcce1857a91
Jona <jbrown6@berklee.edu>
2021-06-16 01:52:36 -0700

    Working project with fake weather API

--- src/App.js
+++ src/App.js
@@ -4,32 +4,44 @@ import React from 'react';
 import CityForm from './CityForm';
 import Header from './Header';
 import Forecast from './Forecast';
+import Footer from './Footer';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      firstForecast: {},
-      secondForecast: {},
-      thirdForecast: {},
+      firstForecast: ['Day', '1'],
+      secondForecast: ['Day', '2'],
+      thirdForecast: ['Day', '3'],
     }
   }
 
-  setForecast = (data) => {
+  setFirstForecast = (data) => {
     this.setState({
-      forecast: data
+      firstForecast: data
     });
   }
 
+  setSecondForecast = (data) => {
+    this.setState({
+      secondForecast: data
+    });
+  }
+
+  setThirdForecast = (data) => {
+    this.setState({
+      thirdForecast: data
+    });
+  }
 
   render() { 
-    console.log(this.state.forecast);
     return (
       <div className="App">
         <Header className="head" />
-        <CityForm className="body" setForecast={this.setForecast}/>
-      <Forecast firstForecast={this.state.forecast} secondForecast={this.state.secondForecast} thirdForecast={this.state.thirdForecast}/>
+        <CityForm className="body" setFirstForecast={this.setFirstForecast} setSecondForecast={this.setSecondForecast} setThirdForecast={this.setThirdForecast} />
+        <Forecast firstForecast={this.state.firstForecast} secondForecast={this.state.secondForecast} thirdForecast={this.state.thirdForecast}/>
+        <Footer/>
       </div>
     )
   }
--- src/Forecast.js
+++ src/Forecast.js
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 class Forecast extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
   render() {
     return (
       <>
-      <h1>Forecast</h1>
-      <p>{this.props.firstForecast}</p>
+      <Card bg="dark" style={{ width: '18rem' }}>
+        <ListGroup variant="flush">
+          <h3>3 Day Forecast </h3>
+          <ListGroup.Item>{`${this.props.firstForecast[0]} - ${this.props.firstForecast[1]}`}</ListGroup.Item>
+          <ListGroup.Item>{`${this.props.secondForecast[0]} - ${this.props.secondForecast[1]}`}</ListGroup.Item>
+          <ListGroup.Item>{`${this.props.thirdForecast[0]} - ${this.props.thirdForecast[1]}`}</ListGroup.Item>
+        </ListGroup>
+      </Card>
       </>
-    )
+    );
   }
+
 }
 
 export default Forecast;
